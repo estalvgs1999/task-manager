7af7c5761354fe2104db1a4e19780dfa0327f19f
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2022-01-27 10:08:36 -0600

    [feature] delete task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,7 +12,7 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((t) => t.id === id);
+    return this.tasks.find((task) => task.id === id);
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -27,5 +27,9 @@ export class TasksService {
 
     this.tasks.push(task);
     return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
